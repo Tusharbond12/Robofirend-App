4b7c64622698eb3d0d9d2f94fae99bd2b23234fa
Tushar <tushar110700@gmail.com>
2020-05-10 00:44:45 +0530

    Updates

---
+++ precache-manifest.8352c00f23b3d46b3977fdf798d09344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fca43da96217daae652a6bae7a50ec2",
+    "url": "/Robofirend-App/index.html"
+  },
+  {
+    "revision": "f8e28639158f9785e076",
+    "url": "/Robofirend-App/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "87d6cecc8152d2fa76c3",
+    "url": "/Robofirend-App/static/css/main.3f2c1470.chunk.css"
+  },
+  {
+    "revision": "f8e28639158f9785e076",
+    "url": "/Robofirend-App/static/js/2.78723f3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofirend-App/static/js/2.78723f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d6cecc8152d2fa76c3",
+    "url": "/Robofirend-App/static/js/main.dca0138e.chunk.js"
+  },
+  {
+    "revision": "acb02c656aa1db624122",
+    "url": "/Robofirend-App/static/js/runtime-main.9b2d2ddc.js"
+  }
+]);
